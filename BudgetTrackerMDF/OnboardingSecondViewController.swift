30fb23abbbd1eeaf643822380cd60ca5d016eb3f
MariaGlushtsova <glushtsova@tut.by>
2023-06-17 15:25:05 +0300

    Add constraints
    Добавление констрейнтов для элементов

--- BudgetTrackerMDF/OnboardingSecondViewController.swift
+++ BudgetTrackerMDF/OnboardingSecondViewController.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class OnboardingSecondViewController: UIViewController {
 
+    // MARK: - Outlets
+    
+    @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var scrollImageView: UIImageView!
+    @IBOutlet weak private var bottomView: UIView!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var textLabel: UILabel!
+    @IBOutlet weak private var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
