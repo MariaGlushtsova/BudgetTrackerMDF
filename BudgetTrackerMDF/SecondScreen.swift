c03404cc3ae14c97c4512e20a1fc7dc0076ff77d
MariaGlushtsova <glushtsova@tut.by>
2023-06-17 13:30:04 +0300

    Create_file_second_screen

---
+++ BudgetTrackerMDF/SecondScreen.swift
@@ -0,0 +1,19 @@
+//
+//  SecondScreen.swift
+//  BudgetTrackerMDF
+//
+//  Created by Admin on 17.06.23.
+//
+
+import Foundation
+import UIKit
+
+class applicationStsrtPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
