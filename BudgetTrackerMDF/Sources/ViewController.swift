8bdaf1bf133ad3453ad57c812dc4ff1bcc489c4a
Mikhail Laty <laty.mischa@gmail.com>
2023-06-17 13:59:17 +0300

    Configured files in project.

--- BudgetTrackerMDF/Sources/ViewController.swift
+++ BudgetTrackerMDF/Sources/ViewController.swift
@@ -9,10 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var labek: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-//        labek.font = UIFont(name: "GTWalsheimPro-Regular", size: 16)
+
     }
 
 
