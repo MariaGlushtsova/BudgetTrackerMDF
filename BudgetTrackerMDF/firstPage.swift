15864bbeb7a168ca6966ad95b59a3914a0f7a294
Amo Ozmanyan <cool.ozmanyan2013@yandex.ru>
2023-06-17 13:08:26 +0300

    class applicationStartPage

--- BudgetTrackerMDF/firstPage.swift
+++ BudgetTrackerMDF/firstPage.swift
@@ -7,4 +7,13 @@
 
 import Foundation
 import UIKit
+class applicationStartPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
 
