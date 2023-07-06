f77cbbe366d69c76d603fbb760f2ee7f1d0b3e70
MariaGlushtsova <glushtsova@tut.by>
2023-07-06 19:23:42 +0300

    Fix conflicts resolve

---
+++ BudgetTrackerMDF/OnboardingSecondViewController.swift
@@ -0,0 +1,28 @@
+//
+//  OnboardingSecondViewController.swift
+//  BudgetTrackerMDF
+//
+//  Created by Admin on 17.06.23.
+//
+
+import UIKit
+
+class OnboardingSecondViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    @IBOutlet weak private var imageView: UIImageView!
+    @IBOutlet weak private var scrollImageView: UIImageView!
+    @IBOutlet weak private var bottomView: UIView!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var textLabel: UILabel!
+    @IBOutlet weak private var nextButton: UIButton!
+    @IBOutlet weak var UIButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
