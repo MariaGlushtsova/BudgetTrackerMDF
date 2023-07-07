7e3047254b7fc28fe7cd619df99cd8df027db03a
Ilia <ilya2491@gmail.com>
2023-07-07 10:23:31 +0300

    Add pageControl

--- BudgetTrackerMDF/Sources/OnboardingThirdViewController.swift
+++ BudgetTrackerMDF/Sources/OnboardingThirdViewController.swift
@@ -15,14 +15,13 @@ class OnboardingThirdViewController: UIViewController {
     @IBOutlet weak private var titleLabel: UILabel!
     @IBOutlet weak private var discriptionLabel: UILabel!
     @IBOutlet weak private var interestedButton: UIButton!
-    @IBOutlet weak private var pageImageView: UIImageView!
-    
+    @IBOutlet weak var pageControl: UIPageControl!
+
     // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         addShadowView()
-
     }
 
     // MARK: - Setup
@@ -32,12 +31,23 @@ class OnboardingThirdViewController: UIViewController {
         childView.layer.shadowOpacity = 0.07
         childView.layer.shadowOffset = CGSize(width: 0, height: 12)
         childView.layer.shadowRadius = 36
+        childView.layer.masksToBounds = false
         childView.layer.shouldRasterize = true
         childView.layer.rasterizationScale = UIScreen.main.scale
     }
 
     // MARK: - Actions
 
-   
-
+    @IBAction func buttonTapped(_ sender: Any) {
+//        pageControl.currentPage += 1
+//
+//        if pageControl.currentPage < pageControl.numberOfPages - 1 {
+//            pageControl.currentPage = pageControl.currentPage
+//        } else {
+//            let viewController = Следующий экран
+//            if let navigator = navigationController {
+//                navigator.pushViewController(viewController, animated: true)
+//            }
+//        }
+    }
 }
