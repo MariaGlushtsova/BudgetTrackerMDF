6aeb959c739b326ad69ca0e534cad09c804384f8
Mikhail Laty <laty.mischa@gmail.com>
2023-08-16 20:23:04 +0300

    Configure customView for navigationBar.

--- BudgetTrackerMDF/Sources/Extensions/CustomNavigationBar.swift
+++ BudgetTrackerMDF/Sources/Extensions/CustomNavigationBar.swift
@@ -5,4 +5,25 @@
 //  Created by Михаил Латий on 16.08.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    func createCustomNavigationBar() -> UIView {
+        let customView = UIView()
+        
+        customView.backgroundColor = .white
+        customView.layer.cornerRadius = 30
+        customView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        customView.translatesAutoresizingMaskIntoConstraints = false
+        // Shadow
+        customView.layer.shadowColor = UIColor.black.cgColor
+        customView.layer.shadowOpacity = 0.3
+        customView.layer.shadowOffset = .zero
+        customView.layer.shadowRadius = 5
+        customView.layer.shouldRasterize = true
+        customView.layer.rasterizationScale = UIScreen.main.scale
+
+        return customView
+    }
+}
+
